539afb220061a86ebe845b258ebd1ead8480f43c
kkk <chinxin999@gmail.com>
2019-10-16 00:32:09 +0800

    commit2

--- client/src/GameCard.js
+++ client/src/GameCard.js
@@ -10,14 +10,52 @@ import {
     Button
 } from 'reactstrap';
 
+import {
+
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter
+} from 'reactstrap';
+
 export class GameCard extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            modal: false,
+            nested: false,
+            close: false
+        };
+
+        this.toggle = this.toggle.bind(this);
+        this.toggleNested = this.toggleNested.bind(this);
+        this.toggleAll = this .toggleAll.bind(this);
+
+    }
+
+    toggle = () => {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+    toggleNested = () => {
+
+        this.setState({
+            nested: !this.state.nested,
+            close: false
+        });
+    }
+
+    toggleAll = () => {
+        this.setState({
+            nested: !this.state.nested,
+            close: true
+        })
     }
 
     render() {
 
-        let {id} = this.props.games.id;
         return (
 
             <div>
@@ -34,6 +72,33 @@ export class GameCard extends Component {
                         >
                             Delete
                         </Button>
+
+                        <div>
+                            <Button color="danger" onClick={this.toggle}>Modal Test</Button>
+                            <Modal isOpen={this.state.modal} toggle={this.toggle} className="modaltest">
+                                <ModalHeader toggle={this.toggle}>{this.props.games.name}</ModalHeader>
+                                <ModalBody>
+                                    <img src={this.props.games.screenshot} width="100%" />
+
+                                    <br />
+
+                                    <Button color="success" onClick={this.toggleNested}>Show Nested Modal</Button>
+
+                                    <Modal isOpen={this.state.nested} toggle={this.toggleNested} onClosed={this.state.close ? this.toggle : undefined}>
+                                        <ModalHeader>Nested Modal title</ModalHeader>
+                                        <ModalBody>Stuff and things</ModalBody>
+                                        <ModalFooter>
+                                            <Button color="primary" onClick={this.toggleNested}>Done</Button>{' '}
+                                        </ModalFooter>
+                                    </Modal>
+
+                                </ModalBody>
+                                <ModalFooter>
+                                    <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+                                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                                </ModalFooter>
+                            </Modal>
+                        </div>
 
                     </CardBody>
                 </Card>
